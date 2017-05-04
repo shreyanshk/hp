dcf7d3d220fe86bc34ef08ec6b114242faa6c6dd
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-05-04 19:04:06 +0530

    report form working

--- Project.py
+++ Project.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "static/userimgs"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/db.sqlite3'
 dbctx = SQLAlchemy(app)
--- ReportPageView.py
+++ ReportPageView.py
@@ -1,11 +1,18 @@
 from flask import Blueprint, render_template, request
 from Project import dbctx
 from dbModels import Item
+import os
 
 view = Blueprint('ReportPageView',
         __name__,
         template_folder='templates',
         )
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @view.route("/report", methods = ["GET", "POST"])
 def report():
@@ -13,4 +20,17 @@ def report():
         return render_template("ReportPage.html")
     elif request.method == "POST":
         f = request.form
-        print(f)
+        if 'image' not in request.files:
+            return render_template("ReportPage.html", err = 0)
+        else:
+            i = request.files['image']
+            if i.filename == '':
+                return render_template("ReportPage.html", err = 1)
+            if i and allowed_file(i.filename):
+                ext = "." + i.filename.split(".")[1]
+                item = Item(f['obj'], f['objdescsrt'], f['objdesclng'], f['location'], ext)
+                dbctx.session.add(item)
+                dbctx.session.commit()
+                i.save(os.path.join('static/usrimgs', str(item.id))+ext)
+                return render_template("ReportPage.html")
+    return render_template("ReportPage.html")
