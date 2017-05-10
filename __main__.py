e1eca7a6efa7554421d2f761d4fc75ab65a1181f
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-05-10 12:04:25 +0530

    __main__ : remove duplicate code goof

--- __main__.py
+++ __main__.py
@@ -2,9 +2,6 @@ from Project import create_app
 
 app = create_app()
 
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
-
 app.jinja_env.auto_reload = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.run(host='0.0.0.0', debug=True)
